a923eb037872179bc2c391f698ff330eecec7882
npsaunders <neilpsaunders@gmail.com>
2022-10-21 09:25:21 -0400

    try deploying to Heroku

--- uppr/uppr/settings.py
+++ uppr/uppr/settings.py
@@ -124,5 +124,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 #for use with Heroku
-# import django_heroku
-# django_heroku.settings(locals())
+import django_heroku
+django_heroku.settings(locals())
