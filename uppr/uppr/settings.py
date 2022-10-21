17353bfbba2aa990cbaa9e7e4731357c42965d6e
npsaunders <neilpsaunders@gmail.com>
2022-10-21 07:15:59 -0400

    Heroku related items

--- uppr/uppr/settings.py
+++ uppr/uppr/settings.py
@@ -121,3 +121,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+#for use with Heroku
+import django_heroku
+django_heroku.settings(locals())
