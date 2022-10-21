284c73c7c79742c27dd423805a37bb2bd7865db8
npsaunders <neilpsaunders@gmail.com>
2022-10-21 07:28:12 -0400

    fix typo in settings.py

--- uppr/uppr/settings.py
+++ uppr/uppr/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = "uppr.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "uppr"",
+        "NAME": "uppr",
     }
 }
 
